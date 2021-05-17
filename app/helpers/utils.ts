2bad5fb3326649692334b37177f4a3f02b53084e
Armand Sallé <armand.salle@gmail.com>
2021-05-17 09:55:50 +0200

    Change import route

--- app/helpers/utils.ts
+++ app/helpers/utils.ts
@@ -1,4 +1,4 @@
-import type { ParsedQuery } from "app/queries/serieQueries"
+import type { ParsedQuery } from "../queries/serieQueries"
 
 export const fromArrayToString = (el: ParsedQuery): string | ParsedQuery | undefined => {
   return Array.isArray(el) ? el[0] : el
